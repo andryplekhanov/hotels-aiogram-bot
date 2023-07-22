77ddf01f316ef7dc72999db68c6c595dbfc742c8
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-07-22 18:33:24 +0300

    edit states

--- tgbot/misc/states.py
+++ tgbot/misc/states.py
@@ -12,10 +12,9 @@ class UsersStates(StatesGroup):
         city_id (str): id города, в котором ищем отели.
         cities (Dict): подходящие по названию города, из которых пользователь выбирает нужный ему.
         amount_hotels (int): количество отелей.
-        need_photo (bool): нужно ли загружать фото.
         amount_photo (int): количество фото.
         start_date (datetime.date): дата заезда в отель.
-        end_date (datetime.date): дата выезда из отеля.
+        amount_nights (int): количество ночей.
         start_price (int): минимальная цена за ночь.
         end_price (int): максимальная цена за ночь.
         end_distance (float): максимальная дистанция до центра города.
@@ -26,10 +25,9 @@ class UsersStates(StatesGroup):
     city_id = State()
     cities = State()
     amount_hotels = State()
-    need_photo = State()
     amount_photo = State()
     start_date = State()
-    end_date = State()
+    amount_nights = State()
     start_price = State()
     end_price = State()
     end_distance = State()
