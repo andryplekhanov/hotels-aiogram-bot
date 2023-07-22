022a21b44dc75fbfa0feb86e1b369f674f389315
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-07-22 15:25:27 +0300

    factory 'for_hotels' added

--- tgbot/services/factories.py
+++ tgbot/services/factories.py
@@ -2,3 +2,4 @@ from aiogram.utils.callback_data import CallbackData
 
 for_city = CallbackData("search", "city_id", "city_name")
 for_photo = CallbackData("photos", "amount")
+for_hotels = CallbackData("hotels", "amount")
