07641d98fc2c11de25e99d9eb60fd5650817b4c9
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-07-24 17:29:49 +0300

    add hotels' result's processor

--- tgbot/services/get_hotels.py
+++ tgbot/services/get_hotels.py
@@ -1,16 +1,21 @@
+import json
 from typing import Union, List
 
 from tgbot.config import Config
 from tgbot.services.api_requests import request_to_api
 
 
-async def parse_hotels(states: dict, config: Config) -> Union[dict[str, List[dict]], None]:
+async def parse_hotels(states: dict, config: Config) -> Union[dict, None]:
     """
     Функция делает запрос в request_to_api и десериализирует результат. Если запрос получен и десериализация прошла -
     возвращает обработанный результат в виде словаря, иначе None.
     """
 
-    sort = 'PRICE_HIGH_TO_LOW' if states.get('last_command') == 'highprice' else 'PRICE_LOW_TO_HIGH'
+    sort = 'PRICE_LOW_TO_HIGH'
+    results_size = int(states.get('amount_hotels'))
+
+    if states.get('last_command') == 'highprice':
+        results_size = 1000
 
     url = config.misc.url_list
     payload = {
@@ -31,11 +36,11 @@ async def parse_hotels(states: dict, config: Config) -> Union[dict[str, List[dic
         },
         "rooms": [
             {
-                "adults": 2
+                "adults": states.get('amount_adults')
             }
         ],
         "resultsStartingIndex": 0,
-        "resultsSize": int(states.get('amount_hotels')),
+        "resultsSize": results_size,
         "sort": sort
     }
 
@@ -46,8 +51,41 @@ async def parse_hotels(states: dict, config: Config) -> Union[dict[str, List[dic
     }
 
     response = await request_to_api(url, headers=headers, querystring=payload, method='POST')
+    if not response:
+        return None
+    try:
+        all_info = json.loads(response.text)
+        hotels_info_list = all_info.get('data', None).get('propertySearch', None).get('properties', None)
+        result = await process_hotels_info(hotels_info_list, states)
+    except Exception:
+        result = None
+    return result
 
-    if response:
-        print(response.json())
-    else:
-        print('none')
+    
+async def process_hotels_info(hotels_info_list: List[dict], states: dict) -> dict[str, dict]:
+    """
+    Функция получает список словарей - результат парсинга отелей, выбирает нужную информацию, обрабатывает и складывает
+    в словарь hotels_info_dict
+    """
+
+    if states.get('last_command') == 'highprice':  # Если была команда 'highprice', то список отелей берем с конца
+        hotels_info_list = hotels_info_list[::-1][:int(states.get('amount_hotels'))]
+
+    hotels_info_dict = dict()
+    for hotel in hotels_info_list:
+        hotel_id = hotel.get('id', None)
+        if not hotel_id:
+            continue
+        hotel_name = hotel.get('name', 'No name')
+        price_per_night = hotel.get('price', {}).get('lead', {}).get('amount', 0)
+        total_price = round(price_per_night * states.get('amount_nights'), 2)
+        distance_city_center = hotel.get('destinationInfo', {}).get('distanceFromDestination', {}).get('value', 0)
+        score = hotel.get('reviews', {}).get('score', 0)
+        hotels_info_dict[hotel_id] = {
+            'name': hotel_name,
+            'price_per_night': price_per_night,
+            'total_price': total_price,
+            'distance_city_center': distance_city_center,
+            'score': score,
+        }
+    return hotels_info_dict
