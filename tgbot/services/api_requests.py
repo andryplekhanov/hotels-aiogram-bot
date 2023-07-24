4ff623355b468af900ef894af92c66c5c2f7f5b9
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-07-24 18:11:19 +0300

    add logging

--- tgbot/services/api_requests.py
+++ tgbot/services/api_requests.py
@@ -1,7 +1,9 @@
 from typing import Dict, Union
-
+import logging
 import requests
 from requests.models import Response
+
+logger = logging.getLogger(__name__)
 
 
 async def request_to_api(
@@ -14,13 +16,17 @@ async def request_to_api(
     Функция осуществляет запрос к api. Если ответ == 200: возвращает результат, иначе None.
     """
 
+    logger.info(f"Starting request to API (method: {method}; url: {url})")
     try:
         if method == 'GET':
             response = requests.get(url=url, params=querystring, headers=headers)
         else:
             response = requests.post(url, json=querystring, headers=headers)
         if response.status_code == requests.codes.ok:
+            logger.info("request OK")
             return response
+        logger.info("Bad request")
         return None
     except Exception:
+        logger.info("Bad request")
         return None
