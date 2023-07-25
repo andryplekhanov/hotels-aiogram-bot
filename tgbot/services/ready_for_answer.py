2820a16ec5dd4eaaf65240910c44fd33a8b7865b
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-07-25 13:27:06 +0300

    fix bugs

--- tgbot/services/ready_for_answer.py
+++ tgbot/services/ready_for_answer.py
@@ -1,11 +1,11 @@
 import logging
 
 from aiogram.dispatcher import FSMContext
+from aiogram.types import Message
+
 from tgbot.config import Config
 from tgbot.keyboards.inline import show_prev_next_callback
 from tgbot.services.get_hotels import parse_hotels, get_hotel_info_str
-from typing import Union
-from aiogram.types import Message
 
 logger = logging.getLogger(__name__)
 
@@ -17,7 +17,7 @@ async def get_prereply_str(state: FSMContext) -> str:
     if states.get('last_command') in ['highprice', 'lowprice']:
         prereply_str = f"✅ Ок, ищу: <b>топ {states['amount_hotels']}</b> " \
                        f"самых {sort_order} отелей в городе <b>{states['city_name']}</b>\n" \
-                       f"{f'Нужно загрузить фото' if states['amount_photo'] > 0 else f'Фото не нужны'}" \
+                       f"{f'Нужно загрузить фото' if int(states['amount_photo']) > 0 else f'Фото не нужны'}" \
                        f" — <b>{states['amount_photo']}</b> штук\n" \
                        f"Длительность поездки: <b>{states['amount_nights']} ноч.</b> " \
                        f"(с {states['start_date'].strftime('%d.%m.%Y')} по {states['end_date'].strftime('%d.%m.%Y')})."
@@ -26,7 +26,7 @@ async def get_prereply_str(state: FSMContext) -> str:
                        f"<b>{states['city_name']}</b>\n" \
                        f"В ценовом диапазоне <b>от {states['start_price']}$ до {states['end_price']}$</b>\n" \
                        f"Максимальная удаленность от центра: <b>{states['end_distance']} Км</b>\n" \
-                       f"{f'Нужно загрузить фото' if states['amount_photo'] else f'Фото не нужны'}" \
+                       f"{f'Нужно загрузить фото' if int(states['amount_photo']) > 0 else f'Фото не нужны'}" \
                        f" — <b>{states['amount_photo']}</b> штук\n" \
                        f"Количество гостей: <b>{states['amount_adults']} взрослых</b>\n" \
                        f"Длительность поездки: <b>{states['amount_nights']} ноч.</b> " \
