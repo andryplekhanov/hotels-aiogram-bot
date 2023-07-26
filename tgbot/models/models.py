2f72656e97798c7bf4a3799a3aa848d6c8e5b173
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-07-26 18:18:39 +0300

    add 'Request' table

--- tgbot/models/models.py
+++ tgbot/models/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime, BigInteger
+from sqlalchemy import Column, Integer, String, DateTime, BigInteger, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -16,3 +16,20 @@ class User(Base):
 
     def __repr__(self):
         return f'{self.tg_id}'
+
+
+class Request(Base):
+    __tablename__ = 'Request'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    user_id = Column(BigInteger, ForeignKey('user.id', ondelete='CASCADE'), nullable=False, index=True)
+    command = Column(String, nullable=False)
+    date = Column(DateTime, default=datetime.now, nullable=False)
+    city_name = Column(String, nullable=False)
+    amount_nights = Column(Integer, nullable=False)
+    amount_adults = Column(Integer, nullable=True)
+    start_date = Column(DateTime, nullable=False)
+    end_date = Column(DateTime, nullable=False)
+    start_price = Column(Integer, nullable=True)
+    end_price = Column(Integer, nullable=True)
+    end_distance = Column(Integer, nullable=True)
